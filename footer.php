052ea752a1cf68de06393a282a3f0fbeffc5aa2e
Vasiliy <kkolyasev1228@gmail.com>
2020-07-10 01:57:51 +0300

    girly

--- footer.php
+++ footer.php
@@ -8,36 +8,6 @@
         <a href="/"><i class="icon-linked_in_circle text-size-25 text-dark"></i></a>
     </div>
     <!-- Main Footer -->
-    <section class="section background-dark">
-        <div class="line">
-            <div class="margin2x">
-                <div class="s-12 m-6 l-3 xl-5">
-                    <h4 class="text-white text-strong">Our Mission</h4>
-                    <p>
-                        <b class="text-size-20">Veri fastidii consectetuer</b> ius in, eum alii dicunt omnium eu. Wisi nostrud equidem ut usu. <b class="text-size-20">Deleniti pertinacia eu est</b>, te his soluta quaestio pericula.
-                    </p>
-                </div>
-                <div class="s-12 m-6 l-3 xl-2">
-                    <h4 class="text-white text-strong margin-m-top-30">Useful Links</h4>
-                    <a class="text-primary-hover" href="page.html">FAQ</a><br>
-                    <a class="text-primary-hover" href="contact.html">Contact Us</a><br>
-                    <a class="text-primary-hover" href="blog.html">Blog</a>
-                </div>
-                <div class="s-12 m-6 l-3 xl-2">
-                    <h4 class="text-white text-strong margin-m-top-30">Term of Use</h4>
-                    <a class="text-primary-hover" href="page.html">Terms and Conditions</a><br>
-                    <a class="text-primary-hover" href="page.html">Refund Policy</a><br>
-                    <a class="text-primary-hover" href="page.html">Disclaimer</a>
-                </div>
-                <div class="s-12 m-6 l-3 xl-3">
-                    <h4 class="text-white text-strong margin-m-top-30">Contact Us</h4>
-                    <p><i class="icon-sli-screen-smartphone text-primary"></i> 0800 4521 800 50</p>
-                    <a class="text-primary-hover" href="mailto:contact@sampledomain.com"><i class="icon-sli-mouse text-primary"></i> contact@sampledomain.com</a><br>
-                    <a class="text-primary-hover" href="mailto:office@sampledomain.com"><i class="icon-sli-mouse text-primary"></i> office@sampledomain.com</a>
-                </div>
-            </div>
-        </div>
-    </section>
     <div class="background-dark">
         <div class="line">
             <hr class="break margin-top-bottom-0" style="border-color: #777;">
--- functions.php
+++ functions.php
@@ -126,12 +126,8 @@ function girly_widgets_init() {
 	register_sidebar(
 		array(
 			'name'          => esc_html__( 'Sidebar', 'girly' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'girly' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
+			'id'            => 'sidebar',
+			'description'   => esc_html__( 'Add widgets here.', 'girly' )
 		)
 	);
 }
