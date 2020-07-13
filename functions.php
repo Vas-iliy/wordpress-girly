fda7ae7cca3d6a1ea28d9d1d551269dfac72d946
Vasiliy <kkolyasev1228@gmail.com>
2020-07-13 15:30:54 +0300

    girly

--- functions.php
+++ functions.php
@@ -216,6 +216,25 @@ function short_description ()
 	echo the_excerpt() . "<br>";
 }
 
+//sidebar woocommerce
+function true_register_wp_sidebars() {
+
+	/* В боковой колонке - первый сайдбар */
+	register_sidebar(
+		array(
+			'id' => 'true_side', // уникальный id
+			'name' => 'Боковая колонка', // название сайдбара
+			'description' => 'Перетащите сюда виджеты, чтобы добавить их в сайдбар.', // описание
+			'before_widget' => '<div id="%1$s" class="side widget %2$s">', // по умолчанию виджеты выводятся <li>-списком
+			'after_widget' => '</div>',
+			'before_title' => '<h3 class="widget-title">', // по умолчанию заголовки виджетов в <h2>
+			'after_title' => '</h3>'
+		)
+	);
+}
+
+add_action( 'widgets_init', 'true_register_wp_sidebars' );
+
 
 require get_template_directory() . '/inc/customizer.php';
 
